f7185c01c4f96542493060e9de4b0618e692a9d2
leoev <leoev@example.com>
2026-07-17 21:37:28 +0700

    style: increase size of LOGIN heading to text-5xl font-extrabold

--- app/[lang]/admin/login/page.tsx
+++ app/[lang]/admin/login/page.tsx
@@ -1,7 +1,7 @@
 export default function AdminLoginPage() {
   return (
     <section className="mx-auto max-w-md px-12 md:px-16 py-12 md:py-16">
-      <h1 className="text-3xl font-semibold text-center">LOGIN</h1>
+      <h1 className="text-5xl font-extrabold tracking-wider text-center">LOGIN</h1>
     </section>
   );
 }
