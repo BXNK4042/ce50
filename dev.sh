90d63a6306c130fca19cdb30aea7cb6681c48354
BXNK4042 <parinthonkongthong@gmail.com>
2026-08-03 10:45:53 +0700

    feat(dev): add 3rd split pane for python backend server

--- dev.sh
+++ dev.sh
@@ -14,6 +14,9 @@ tmux new-session -d -s "$SESSION" -n "dev-tunnel" "npm run dev; exec \$SHELL"
 # Split window horizontally & run ngrok http 3000
 tmux split-window -h -t "$SESSION" "ngrok http 3000; exec \$SHELL"
 
+# Split window vertically for backend server
+tmux split-window -v -t "$SESSION" "cd server && uvicorn main:app --reload --port 8000; exec \$SHELL"
+
 # Evenly space panes & attach
-tmux select-layout -t "$SESSION" tile
+tmux select-layout -t "$SESSION" tiled
 exec tmux attach-session -t "$SESSION"
